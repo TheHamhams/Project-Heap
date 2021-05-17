cd3dd6b1b40e2fb609aa51b4ae1a83e71e9bc280
Hamhams <hamhams86@gmail.com>
2021-05-17 07:25:32 -0700

    Add method added, tested with 2 adds

--- heap.py
+++ heap.py
@@ -3,3 +3,18 @@ class MaxHeap():
         self.count = 0
         self.heap_list = [None]
         self.name = name
+    
+    def add(self, value):
+        self.heap_list.append(value)
+        if self.heap_list[1] != value:
+            self.heapify_up
+        print(f"{value} added to {self.name}, {self.heap_list}")
+        self.count += 1
+        return 
+
+    def heapify_up():
+        print("Heapifying up")
+
+test = MaxHeap("test")
+test.add({"sweep": 1})
+test.add({"vacuum": 2})
